2f6a452225739cefc4cd0fb53ff35963a8b3ada6
marinachennn <marinachen07@gmail.com>
2023-12-02 16:12:31 -0800

    Power Meter

--- Assets/Scripts/PowerMetreScript.cs
+++ Assets/Scripts/PowerMetreScript.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerMetreScript : MonoBehaviour
 {
     [SerializeField] Rigidbody2D powerMeter;
     private int power = 0;
-
+    private float power0 = 1.0f;
+    public Image fillMeter;
+    
     // Start is called before the first frame update
     void Start()
     {
-        powerMeter.position = new Vector2(-3, 0);
+        powerMeter.position = new Vector2(-5, 3);
     }
 
+
     // Update is called once per frame
-    void Update()
+    void Update()1
     {
-        Debug.Log(power);
+        Debug.Log(fillMeter.fillAmount);
 
         if (Input.GetKey(KeyCode.Space)) {
             power++;
@@ -26,6 +30,10 @@ public class PowerMetreScript : MonoBehaviour
                 power--;
             }
         }
+
+        if (power <= 300) power0 = 1 - ((float)power / 300);
         
+        //Reduce fill amount over 30 seconds
+        fillMeter.fillAmount = power0;
     }
 }
